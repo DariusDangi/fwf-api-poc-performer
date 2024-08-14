1e902c53add7fdc8e0ca3b13e61bf9c0e3e7c805
DariusDangi <darius.dangi@fwfcompany.com>
2024-08-14 10:16:03 +0300

    Initial commit to dev branch

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace fwfapipocperformer
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
